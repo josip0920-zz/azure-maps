c56caa1893e0f776a3386cf5b631be31c9d28e75
Josip <eugene20920@gmail.com>
2022-08-04 21:26:38 +0900

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Leaf from "./Leaf";
 //import classes from "./app.module.css";
 import { Container } from "react-bootstrap";
-
+import GEOS from './data.json';
 //UTIL IMPORTS
 //import { compare } from './util';
 
@@ -12,43 +12,44 @@ const Home = () => {
     const viewPort = {
         height: "100vh",
         width: "100vw",
-        latitude: 40.751,
-        longitude: -73.9688,
+        latitude: -41.5275314820192,
+        longitude: 173.718680632181,
         zoom: 11
     };
-    const [loaded, setLoaded] = useState(1)
-    const [geojson, setGeojson] = useState(null)
+
+    // const [loaded, setLoaded] = useState(1)
+    // const [geojson, setGeojson] = useState(geos)
     // const [data, setData] = useState([])
 
     const ref = useRef();
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+    // useEffect(() => {
+    //     fetchData()
+    // }, [])
 
 
-    const fetchData = () => {
+    // const fetchData = () => {
 
-        if (loaded === 1) {
-            fetch(
-                "https://raw.githack.com/datafaust/raw/main/cruise-prototype/hh_2020112300_2020120623_Saturday_02.geojson"
-            )
-                .then((response) => response.json())
-                .then((geojson) => {
-                    setGeojson(geojson);
-                    setLoaded(2);
-                });
-        } else {
-            fetch(
-                "https://raw.githack.com/datafaust/raw/main/cruise-prototype/hh_2020112300_2020120623_Saturday_03.geojson"
-            )
-                .then((response) => response.json())
-                .then((geojson) => {
-                    setGeojson(geojson);
-                    setLoaded(1);
-                });
-        }
-    };
+    //     if (loaded === 1) {
+    //         fetch(
+    //             "https://raw.githack.com/datafaust/raw/main/cruise-prototype/hh_2020112300_2020120623_Saturday_02.geojson"
+    //         )
+    //             .then((response) => response.json())
+    //             .then((geojson) => {
+    //                 setGeojson(geos);
+    //                 setLoaded(2);
+    //             });
+    //     } else {
+    //         fetch(
+    //             "https://raw.githack.com/datafaust/raw/main/cruise-prototype/hh_2020112300_2020120623_Saturday_03.geojson"
+    //         )
+    //             .then((response) => response.json())
+    //             .then((geojson) => {
+    //                 setGeojson(geojson);
+    //                 setLoaded(1);
+    //             });
+    //     }
+    // };
 
     // const geoFilter = (feature) => {
     //     let ids = [0, 1, 5, 6];
@@ -62,13 +63,13 @@ const Home = () => {
 
     return (
         <Container fluid="md" style={{ height: '100%' }}>
-            {geojson && (
+            {GEOS && (
                 <Leaf
                     setRef={ref}
                     // data={data}
-                    fetchData={fetchData}
+                    // fetchData={fetchData}
                     viewPort={viewPort}
-                    geojson={geojson}
+                    geojson={GEOS}
                 />
             )}
         </Container>
