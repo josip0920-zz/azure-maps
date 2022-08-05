56bf693bae0fb0dfc12dcc281a77dff5fd16f724
Josip <eugene20920@gmail.com>
2022-08-05 23:39:13 +0900

    Add button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useRef, useState } from "react";
 import List from '@mui/material/List';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import Checkbox from '@mui/material/Checkbox';
 import ListItem from '@mui/material/ListItem';
@@ -27,16 +28,23 @@ const Home = () => {
     const ref = useRef();
     const [searchKey, setSearchKey] = useState('');
     const [checked, setChecked] = useState([]);
+    const [showAll, setShowAll] = useState(true)
 
     const searchedItem = useMemo(() => {
         setChecked([]);
+
         if (!searchKey) {
-            return [];
+            if (showAll) {
+                const _item = GEOS.features.map(item => item);
+                return _item;
+            } else {
+                return [];
+            }
         }
 
         const _item = GEOS.features.filter(item => String(item.properties.name).toLowerCase().search(String(searchKey).toLowerCase()) != -1);
         return _item;
-    }, [searchKey])
+    }, [searchKey, showAll])
 
     const handleCheck = (id) => {
         setChecked(prev => {
@@ -55,36 +63,51 @@ const Home = () => {
         <Container fluid="md" style={{ height: '100%' }}>
             <Stack direction="row" sx={{ height: '100%' }}>
                 <Stack sx={{ width: 300 }}>
-                    <Stack sx={{ p: 2 }} spacing={2}>
-                        <Typography fontWeight={500}>
-                            Block Search
-                        </Typography>
-                        <CInput placeholder="Search" value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
-                        <Stack direction='row' justifyContent="space-between">
-                            <IconButton>
-                                <FilterListIcon />
-                            </IconButton>
-                            <IconButton>
-                                <BlurOnIcon />
-                            </IconButton>
+                    <Stack flexGrow={1}>
+                        <Stack sx={{ p: 2 }} spacing={2}>
+                            <Typography fontWeight={500}>
+                                Block Search
+                            </Typography>
+                            <CInput placeholder="Search" value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
+                            <Stack direction='row' justifyContent="space-between">
+                                <IconButton>
+                                    <FilterListIcon />
+                                </IconButton>
+                                <IconButton>
+                                    <BlurOnIcon />
+                                </IconButton>
+                            </Stack>
                         </Stack>
+                        <List component="nav" aria-label="mailbox folders">
+                            <Divider />
+                            {
+                                searchedItem.map((item, index) => (
+                                    <Stack key={index}>
+                                        <ListItem button onClick={() => handleCheck(item.properties.id)}>
+                                            <Checkbox checked={Boolean(checked.indexOf(item.properties.id) != -1)} />
+                                            <Typography sx={{ mt: '1px' }}>{item.properties.name}</Typography>
+                                        </ListItem>
+                                        <Divider />
+                                    </Stack>
+
+                                ))
+                            }
+                        </List>
+
                     </Stack>
-                    <List component="nav" aria-label="mailbox folders">
-                        <Divider />
-                        {
-                            searchedItem.map((item, index) => (
-                                <Stack key={index}>
-                                    <ListItem button onClick={() => handleCheck(item.properties.id)}>
-                                        <Checkbox checked={Boolean(checked.indexOf(item.properties.id) != -1)} />
-                                        <Typography sx={{ mt: '1px' }}>{item.properties.name}</Typography>
-                                    </ListItem>
-                                    <Divider />
-                                </Stack>
-
-                            ))
-                        }
-                    </List>
-
+                    <Stack
+                        direction="row"
+                        justifyContent='space-between'
+                        sx={{
+                            p: 2,
+                            '& button': {
+                                color: 'green'
+                            }
+                        }}
+                    >
+                        <Button variant="text" onClick={() => setShowAll(false)}>HIDE ALL</Button>
+                        <Button variant="text" onClick={() => setShowAll(true)}>SHOW ALL</Button>
+                    </Stack>
                 </Stack>
                 {GEOS && (
                     <Leaf
